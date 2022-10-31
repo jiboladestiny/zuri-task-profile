2ca3db892505b2e42c57917808b678d5985a7cf9
jiboladestiny <jibola1444@gmail.com>
2022-10-31 14:38:16 +0000

    added profile links

---
+++ src/components/Profilelink.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+const Profilelink = () => {
+  return (
+    <div className="profilelink mt-5">
+      <div class="d-grid gap-3">
+        <button
+          onClick={() => {
+            window.location.href = "https://droomsoccer.com/";
+          }}
+          class="btn btn-lg btn-primary"
+          type="button"
+        >
+          Button
+        </button>
+        <button
+          onClick={() => {
+            window.location.href = "https://droomsoccer.com/";
+          }}
+          class="btn btn-lg btn-primary"
+          type="button"
+        >
+          Button
+        </button>
+        <button
+          onClick={() => {
+            window.location.href = "https://droomsoccer.com/";
+          }}
+          class="btn btn-lg btn-primary"
+          type="button"
+        >
+          Button
+        </button>{" "}
+        <button
+          onClick={() => {
+            window.location.href = "https://droomsoccer.com/";
+          }}
+          class="btn btn-lg btn-primary"
+          type="button"
+        >
+          Button
+        </button>{" "}
+        <button
+          onClick={() => {
+            window.location.href = "https://droomsoccer.com/";
+          }}
+          class="btn btn-lg btn-primary"
+          type="button"
+        >
+          Button
+        </button>{" "}
+        <button
+          onClick={() => {
+            window.location.href = "https://droomsoccer.com/";
+          }}
+          class="btn btn-lg btn-primary"
+          type="button"
+        >
+          Button
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Profilelink;
